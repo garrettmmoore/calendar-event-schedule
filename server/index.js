b00161de35bdc0e7d3ae5ac15b77347807d82805
Garrett Moore <garrettmarkmoore@gmail.com>
2020-05-23 02:02:16 -0700

    Add express server to use in place of serverless

---
+++ server/index.js
@@ -0,0 +1,26 @@
+// const express = require("express");
+// const ical = require("node-ical");
+// const dotenv = require("dotenv");
+
+// const app = express();
+// dotenv.config();
+
+// const port = process.env.PORT || 3000;
+// const calendarURL = process.env.CALENDAR_URL;
+
+// app.get("/", (req, res) => {
+//   res.send("hello world");
+// });
+
+// app.get("/calendar", (_req, res) => {
+//   // const cal = ical.fromURL(`${calendarURL}`, (error, data) => {
+//   //   if (error) {
+//   //     res.send(error);
+//   //   }
+//   //   res.send(data);
+//   // });
+// });
+
+// app.listen(port, () =>
+//   console.log(`App listening at http://localhost:${port}`)
+// );
